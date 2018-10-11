06ff1a26a7dd445ac53f8ecfe88523d10e5d41c1
Lisa Martens <lisa@Lisas-MacBook-Pro.local>
2018-10-11 11:53:30 +0200

    fix: prettier + correct type

--- src/computed/utils/textAlternative/tooltip.ts
+++ src/computed/utils/textAlternative/tooltip.ts
@@ -1,20 +1,24 @@
-import Roletype from "../../../role/Roletype";
+import Roletype from '../../../role/Roletype';
 import textAlternative from './../textAlternative';
 
 export function has(current: Roletype) {
-	return (current.element instanceof HTMLElement && (current.element as HTMLElement).title) ||
-		(current.element instanceof SVGElement && current.element.querySelector(":scope > title"));
+  return (
+    (current.element instanceof HTMLElement &&
+      (current.element as HTMLElement).title) ||
+    (current.element instanceof SVGElement &&
+      current.element.querySelector(':scope > title'))
+  );
 }
 
-export function get(current: Roletype) : String {
-	if(current.element instanceof HTMLElement){
-		return (current.element as HTMLElement).title.trim();
-	} else if(current.element instanceof SVGElement) {
-		const title = current.element.querySelector(":scope > title");
-		return Array.prototype
-			.map.call(title.childNodes, node => textAlternative(node, current))
-			.reduce((prev, current) => current ? prev + current : prev, '');;
-	}
+export function get(current: Roletype): string {
+  if (current.element instanceof HTMLElement) {
+    return (current.element as HTMLElement).title.trim();
+  } else if (current.element instanceof SVGElement) {
+    const title = current.element.querySelector(':scope > title');
+    return Array.prototype.map
+      .call(title.childNodes, node => textAlternative(node, current))
+      .reduce((prev, current) => (current ? prev + current : prev), '');
+  }
 }
 
 export default { has, get };
