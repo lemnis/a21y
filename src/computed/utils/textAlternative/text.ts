84b571557e48e9860a2112b3e744136611871661
Lisa Martens <lisa@Lisas-MacBook-Pro.local>
2018-10-11 11:54:57 +0200

    feat: added return types & prettier

--- src/computed/utils/textAlternative/text.ts
+++ src/computed/utils/textAlternative/text.ts
@@ -1,11 +1,9 @@
-import Roletype from "../../../role/Roletype";
-
-export function is(node: Node) {
-	return node instanceof Text;
+export function is(node: Node): boolean {
+  return node instanceof Text;
 }
 
-export function get(node: Node){
-	return (node as any).textContent.trim() || null;
+export function get(node: Node): string {
+  return (node as any).textContent.trim() || null;
 }
 
 export default { is, get };
