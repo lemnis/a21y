2496230b329a26265764c72ce5930a33ba6eddf5
Lisa Martens <lisa@Lisas-MacBook-Pro.local>
2018-10-11 11:57:48 +0200

    fix: prettier

--- src/computed/utils/textAlternative/nativeTextAlternative.ts
+++ src/computed/utils/textAlternative/nativeTextAlternative.ts
@@ -1,42 +1,41 @@
-import Roletype from "../../../role/Roletype";
+import Roletype from '../../../role/Roletype';
 
 // First linked element with e.g. labels
 // then strings
 
 function has(current: Roletype, traversedElements: Element[]): Boolean {
-	if (
-		(current.element as any).labels && (current.element as any).labels.length > 0 &&
-		traversedElements.indexOf(current.element) === -1
-	) {
-		return true;
-	}
+  if (
+    (current.element as any).labels &&
+    (current.element as any).labels.length > 0 &&
+    traversedElements.indexOf(current.element) === -1
+  ) {
+    return true;
+  }
 
-	if (
-		current.element instanceof HTMLElement &&
-		!!current.element.title
-	) {
-		return true;
-	}	
+  if (current.element instanceof HTMLElement && !!current.element.title) {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
-function get(current: Roletype, traversedElements: Element[]): String | Element[] {
-	
-	if (
-		(current.element as any).labels && (current.element as any).labels.length > 0 &&
-		traversedElements.indexOf(current.element) === -1
-	) {
-		return Array.from(((current.element as any).labels as Element[]))
-		.filter(element => traversedElements.indexOf(element) === -1);
-	}
+function get(
+  current: Roletype,
+  traversedElements: Element[]
+): String | Element[] {
+  if (
+    (current.element as any).labels &&
+    (current.element as any).labels.length > 0 &&
+    traversedElements.indexOf(current.element) === -1
+  ) {
+    return Array.from((current.element as any).labels as Element[]).filter(
+      element => traversedElements.indexOf(element) === -1
+    );
+  }
 
-	if (
-		current.element instanceof HTMLElement &&
-		!!current.element.title
-	) {
-		return current.element.title.trim();
-	}
+  if (current.element instanceof HTMLElement && !!current.element.title) {
+    return current.element.title.trim();
+  }
 }
 
-export default {has, get};
+export default { has, get };
