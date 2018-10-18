45bdc6a24ea4f3c2053f3d273cdb4199858c2440
Lisa Martens <lisa@Lisas-MacBook-Pro.local>
2018-10-18 17:13:06 +0200

    feat: migrated to prettier & lit-html

--- tests/computed/description.ts
+++ tests/computed/description.ts
@@ -1,315 +1,199 @@
 /// <reference path='../../node_modules/@types/chai/index.d.ts' />
 import Roletype from './../../src/role/Roletype';
+import { html, render } from '../../node_modules/lit-html/lit-html';
 
 export default () => {
-	describe('description', () => {
-		let node: HTMLElement;
-
-		beforeEach(() => {
-			node = document.createElement('div');
-			node.innerHTML = 'foo';
-			document.body.appendChild(node);
-		});
-
-		afterEach(() => {
-			document.body.removeChild(node);
-		});
-
-		it('1.0 combobox focusable', async () => {
-			node.setAttribute('role', 'combobox');
-			node.tabIndex = 0;
-			node.title = 'Choose your language.';
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-			chai.expect(description).to.equal(null);
-		});
-
-		it('from content of describedby element', async () => {
-			node.setAttribute('aria-label', 'Important stuff');
-			node.setAttribute('aria-describedby', 'ID1');
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `
-				<span aria-hidden="true"><i> Hello, </i></span>
-				<span>My</span> name is
-				<div><img src="#" title="Bryan" alt="" role="presentation" /></div>
-				<span role="presentation" aria-label="Eli">
-					<span aria-label="Garaventa">Zambino</span>
-				</span>
-				<span>the weird.</span>
-				(QED)
-				<span class="hidden"><i><b>and don't you forget it.</b></i></span>
-				<table>
-					<tr>
-						<td>Where</td>
-						<td style="visibility:hidden;"><div>in</div></td>
-						<td><div style="display:none;">the world</div></td>
-						<td>are my marbles?</td>
-					</tr>
-				</table>
-			`;
-			document.body.appendChild(descElement);
-
-			const style = document.createElement('style');
-			style.innerHTML = `.hidden { display: none; }`;
-			document.head.appendChild(style);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-			document.head.removeChild(style);
-
-			chai.expect(description).to.equal('My name is Eli the weird. (QED) Where are my marbles?');
-		});
-
-		it('with label', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('a');
-			(node as HTMLAnchorElement).href = '#';
-			node.title = 'San Francisco';
-			node.setAttribute('aria-label', 'California');
-			node.innerHTML = 'United States';
-			document.body.appendChild(node);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			chai.expect(description).to.equal('San Francisco');
-		});
-
-		it('with alt, title and label - 557', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).src = '#';
-			(node as HTMLImageElement).alt = 'a';
-			node.title = 't';
-			node.setAttribute('aria-label', '1');
-			document.body.appendChild(node);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			chai.expect(description).to.equal('t');
-		});
-
-		it('with reference - 664', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with hidden reference - 665', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			descElement.style.display = 'none';
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with presentational reference - 666', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			descElement.setAttribute('role', 'presentation');
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with alt and reference - 772', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).src = '#';
-			(node as HTMLImageElement).alt = 'test';
-			node.setAttribute('aria-describedby', 't1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 't1';
-			descElement.innerHTML = `foo`;
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with alt and hidden reference - 773', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).alt = 'test';
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			descElement.style.display = 'none';
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with alt and presentational reference - 774', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).alt = 'test';
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			descElement.style.display = 'none';
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with alt and visually hidden reference - 838', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).alt = 'test';
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `foo`;
-			descElement.style.visibility = 'hidden';
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with broken reference', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).alt = 'test';
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 't1');
-			document.body.appendChild(node);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			chai.expect(description).to.equal(null);
-		});
-
-		it('with one valid reference', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('img');
-			(node as HTMLImageElement).alt = 'test';
-			(node as HTMLImageElement).src = '#';
-			node.setAttribute('aria-describedby', 't1 t2 t3');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 't2';
-			descElement.innerHTML = `foo`;
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('foo');
-		});
-
-		it('with title and reference', async () => {
-			document.body.removeChild(node);
-
-			node = document.createElement('input');
-			node.title = 'Title';
-			node.setAttribute('aria-label', 'Name');
-			node.setAttribute('aria-describedby', 'ID1');
-			document.body.appendChild(node);
-
-			const descElement = document.createElement('div');
-			descElement.id = 'ID1';
-			descElement.innerHTML = `Description`;
-			document.body.appendChild(descElement);
-
-			const roletype = new Roletype(node);
-			const description = await roletype.computed.description();
-
-			document.body.removeChild(descElement);
-
-			chai.expect(description).to.equal('Description');
-		});
-	});
-}
+  describe.only('description', () => {
+    let container: HTMLDivElement;
+    let style: HTMLStyleElement;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      document.body.appendChild(container);
+      style = document.createElement('style');
+      style.innerHTML = `.hidden { display: none; }`;
+      document.head.appendChild(style);
+    });
+
+    afterEach(() => {
+      document.body.removeChild(container);
+      document.head.removeChild(style);
+    });
+
+    it('1.0 combobox focusable', async () => {
+      const source = html`
+        <div id='test' role='combobox' tabindex='0'
+        title='Choose your language.'></div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal(null);
+    });
+
+    it('from content of describedby element', async () => {
+      const source = html`
+        <div id='test' aria-label='Important stuff' aria-describedby='ID1'></div>
+        <div id='ID1'>
+          <span aria-hidden="true"><i> Hello, </i></span>
+          <span>My</span> name is
+          <div><img src="#" title="Bryan" alt="" role="presentation" /></div>
+          <span role="presentation" aria-label="Eli">
+            <span aria-label="Garaventa">Zambino</span>
+          </span>
+          <span>the weird.</span>
+          (QED)
+          <span class="hidden"><i><b>and don't you forget it.</b></i></span>
+          <table>
+            <tr>
+              <td>Where</td>
+              <td style="visibility:hidden;"><div>in</div></td>
+              <td><div style="display:none;">the world</div></td>
+              <td>are my marbles?</td>
+            </tr>
+          </table>
+        </div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai
+        .expect(description)
+        .to.equal('My name is Eli the weird. (QED) Where are my marbles?');
+    });
+
+    it('with label', async () => {
+      const source = html`
+        <a id='test' href='#' aria-label='California' title='San Francisco'>
+          United States
+        </a>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('San Francisco');
+    });
+
+    it('with alt, title and label - 557', async () => {
+      const source = html`
+        <img id='test' src='#' alt='a' title='t' aria-label='1' />
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('t');
+    });
+
+    it('with reference - 664', async () => {
+      const source = html`
+        <img id='test' src='#' aria-describedby='ID1' />
+        <div id='ID1'>foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with hidden reference - 665', async () => {
+      const source = html`
+        <img id='test' src='#' aria-describedby='ID1' />
+        <div id='ID1' style='display: none'>foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with presentational reference - 666', async () => {
+      const source = html`
+        <img id='test' src='#' aria-describedby='ID1' />
+        <div id='ID1' role='presentation'>foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with alt and reference - 772', async () => {
+      const source = html`
+        <img id='test' src='#' alt='test' aria-describedby='t1' />
+        <div id='t1'>foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with alt and hidden reference - 773', async () => {
+      const source = html`
+        <img src="#" id="test" alt="test" aria-describedby="t1">
+        <div id="t1" style="display:none">foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with alt and presentational reference - 774', async () => {
+      const source = html`
+        <img src="#" id="test" alt="test" aria-describedby="t1">
+        <span id="t1" role="presentation">foo</span>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with alt and visually hidden reference - 838', async () => {
+      const source = html`
+        <img src="#" id="test" alt="test" aria-describedby="t1">
+        <div id="t1" style="visibility:hidden">foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with broken reference', async () => {
+      const source = html`
+        <img src="#" id="test" alt="test" aria-describedby="t1">
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal(null);
+    });
+
+    it('with one valid reference', async () => {
+      const source = html`
+        <img src="#" id="test" alt="test" aria-describedby="t1 t2 t3">
+        <div id="t2">foo</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('foo');
+    });
+
+    it('with title and reference', async () => {
+      const source = html`
+        <div>
+          <input aria-label="Name" id="test" title="Title"
+            aria-describedby="ID1" type="text">
+        </div>
+        <div id="ID1">Description</div>
+		  `;
+      render(source, container);
+      const roletype = new Roletype(container.querySelector('#test'));
+      const description = await roletype.computed.description();
+      chai.expect(description).to.equal('Description');
+    });
+  });
+};
