5e82bc2e488c8362de913174aa08522962dfa3fb
Lisa Martens <lisa@Lisas-MacBook-Pro.local>
2018-10-19 00:19:55 +0200

    feat: prettier + removed quotes

--- src/computed/utils/getPseudoContent.ts
+++ src/computed/utils/getPseudoContent.ts
@@ -1,52 +1,85 @@
-import Quotes from './quotes';
+/** Very limited implementation of the CSS Level 2 specification */
 
-export default function(node: Element, pseudo: '::before' | '::after'): string {
-  let computedStyles = window.getComputedStyle(node, pseudo);
-  let rawContent: string = computedStyles.content;
+const contentList = {
+  string: { regex: '(^|[^\\(])"(.*?[^\\\\])"', amountOfGroups: 2, number: 2 },
+  // uri: { regex: 'url\((.+?)\)', amountOfGroups: 1, number: 1},
+  // counter: {},
+  attribute: { regex: 'attr\\((.+?)\\)', amountOfGroups: 1, number: 1 }
+  // quote: { regex: '((no-)?(open|close)-quote)', amountOfGroups: 3, number: 1 }
+};
 
-  if (rawContent === 'normal' || rawContent === 'none') {
-    /** @todo: more full featured return of normal & none; or better documentation why they
-     * why they both got null.
-     */
-    return null;
+const innerRegex = Object.keys(contentList).reduce(
+  (acc, key) => (acc += contentList[key].regex + '|'),
+  ''
+);
+const regex = new RegExp(`(${innerRegex})`, 'g');
+
+export default async function(
+  node: Element,
+  pseudo: '::before' | '::after'
+): Promise<string> {
+  const rawContent: string = window.getComputedStyle(node, pseudo).content;
+
+  // with 'normal' the psuedo element is not generated.
+  // 'none' computes to 'normal' within psuedo elements.
+  if (rawContent === 'normal' || rawContent === 'none') return null;
+
+  // more extended implementation for browser who support it
+  if (rawContent && window.getComputedAccessibleNode) {
+    const parentAccessibleNode = await window.getComputedAccessibleNode(node);
+    const pseudoAccessibleNode =
+      pseudo === '::before'
+        ? parentAccessibleNode.firstChild
+        : parentAccessibleNode.lastChild;
+    
+    // check if element is exposed
+    if (pseudoAccessibleNode) {
+      // check if doesn't have multiple accessible children.
+      if (!pseudoAccessibleNode.firstChild) return pseudoAccessibleNode.name;
+
+      let current = pseudoAccessibleNode.firstChild;
+      let result = [];
+
+      while (current) {
+        result.push(current.name);
+        current = current.nextSibling;
+      }
+
+      return result.filter(text => text).join('');
+    }
   }
 
-  /** @todo: Improve splittedContext */
-  let regex = /(".*?[^\\]")/g;
-  let firstIndex = 0;
-  let splittedTextContent = [];
-
-  while (regex.exec(rawContent) !== null) {
-    splittedTextContent.push(rawContent.substring(firstIndex, regex.lastIndex));
-    firstIndex = regex.lastIndex;
+  if(node.computedName) {
+    console.log("PASSED THE ACCESSIBLE NODE", node.computedName, rawContent);
   }
-  if (firstIndex !== rawContent.length)
-    splittedTextContent.push(rawContent.substring(firstIndex));
 
-  let splittedContent = [];
-  splittedTextContent.forEach(str => {
-    if (str.startsWith('"')) {
-      splittedContent.push(str);
-    } else {
-      splittedContent = splittedContent.concat(str.trim().split(' '));
-    }
-  });
+  const result = [];
+  let match;
 
-  function convertString(string: string) {
-    let quotes = new Quotes(computedStyles.quotes);
-    if (quotes.is(string)) {
-      return quotes.get(string);
-    }
+  while ((match = regex.exec(rawContent)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (match.index === regex.lastIndex) regex.lastIndex++;
 
-    // check for images
-    if (string.match(/^url\((.*)\)$/g)) {
-    }
+    let previousLength = 1; // Start at 1, because full regex is encapsultated by one group.
+    for (let key in contentList) {
+      const index = previousLength + contentList[key].number;
 
-    // check for strings
-    if (string.match(/^"(.*)"$/g)) {
-      return string.substring(1, string.length - 1);
+      if (match[index]) {
+        switch (key) {
+          case 'string':
+            result.push(match[index]);
+            break;
+          case 'attribute':
+            result.push(node.getAttribute(match[index]));
+            break;
+          default:
+            break;
+        }
+      }
+
+      previousLength += contentList[key].amountOfGroups;
     }
   }
 
-  return splittedContent.map(convertString).join(' ') || null;
+  return result.join('') || null;
 }
